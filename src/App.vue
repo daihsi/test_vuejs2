aba852eaa95e9a201fcc1dc94ad9931f4d919f26
daihsi <isitomo.mm@gmail.com>
2020-11-08 18:05:25 +0900

    Add: 動的コンポーネント記述確認

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <div>
     <LikeHeader>
-      <template v-slot:title='slotProps'>
         <h2>こんにちは</h2>
-        <p>{{ slotProps.user.firsName }}</p>
-      </template>
-      <template v-slot:number>
-        <h2>{{ number }}</h2>
-      </template>
     </LikeHeader>
     <LikeNumber :total-number='number' @my-click='increment'></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import LikeHeader from './components/LikeHeader.vue';
+import About from './components/About.vue';
+import Home from './components/Home.vue';
 
 export default {
   data() {
     return {
-      number: 10
+      number: 10,
+      currentComponent: 'Home',
     };
   },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home,
   },
   methods: {
     increment(value) {
