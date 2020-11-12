500099b5f07c2b831335b06ac7fb17ea87fc9518
daihsi <isitomo.mm@gmail.com>
2020-11-13 05:37:39 +0900

    Add: スライド・トランジション

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div class="main">
     <button @click="show = !show">切り替え</button>
-    <transition name="fade">
+    <button @click="myComponent = 'ComponentA'">ComponentA</button>
+    <button @click="myComponent = 'ComponentB'">ComponentB</button>
+    <transition name="fade" mode="out-in">
+      <component :is="myComponent"></component>
+    </transition>
+    <transition name="fade" mode="out-in">
+      <p v-if="show" key="bye">さようなら</p>
+      <p v-else key="hello">こんにちは</p>
+    </transition>
+    <transition 
+      enter-active-class="animate__animated animate__bounce" 
+      leave-active-class="animate__animated animate__swing" 
+      appear>
       <p v-if="show">hello!!</p>
     </transition>
-    
+    <transition name="slide" appear>
+      <p v-show="show">bye</p>
+    </transition>
   </div>
 </template>
 
 <script>
+import ComponentA from './components/ComponentA.vue';
+import ComponentB from './components/ComponentB.vue';
+
 export default {
   data() {
     return {
       show: true,
+      myComponent: 'ComponentA'
     };
+  },
+  components: {
+    ComponentA,
+    ComponentB
   }
 }
 </script>
@@ -48,6 +70,30 @@ export default {
   /* 消える時の最後の状態 */
   .fade-leave-to {
     opacity: 0;
+  }
+
+  .slide-entrer,
+  .slide-leave-to {
+    opacity: 0;
+  }
+
+  .slide-entrer-active {
+    animation: slide-in 0.5s;
+    transition: opacity 0.5s;
+  } 
+
+  .slide-leave-active {
+    animation: slide-in 0.5s reverse;
+    transition: opacity 0.5s;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateX(100px);
+    }
+    to {
+      transform: translateX(0);
+    }
   }
 
   .main {
